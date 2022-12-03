235406999e0bf98fdcd9336437b0747894b051cc
TreeSun77 <TreeSun77@TreeSun77.ru>
2022-12-03 18:22:58 +0300

    Move player

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -5,8 +5,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable {
-	// SCREEN SETTINGS
 
+	// SCREEN SETTINGS
 	final  int originalTileSize = 16;
 
 	final  int scale = 3;
@@ -19,6 +19,18 @@ public class GamePanel extends JPanel implements Runnable {
 	final int screenWidth = tileSize * maxScreenCol; // 768
 	final int screenHeight = tileSize * maxScreenRow; // 576
 
+	// PLAYER POSITION
+	int playerX = 110;
+	int playerY = 110;
+	int playerSpeed = 4;
+
+	int FPS = 60;
+
+	KeyHandler keyH = new KeyHandler();
+
+
+
+
 
 	Thread gameThread;
 
@@ -26,6 +38,8 @@ public class GamePanel extends JPanel implements Runnable {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.BLACK);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 	public void  startGameThread(){
 		gameThread = new Thread( this);
@@ -34,6 +48,53 @@ public class GamePanel extends JPanel implements Runnable {
 	//RUN
 	@Override
 	public void run() {
+		while (gameThread!= null){
+			double drawInterval = 1000000000/FPS;
+			double nextDrawTime = System.nanoTime() + drawInterval;
+			update();
+			repaint();
+
+			try {
+					double remainingTime = nextDrawTime - System.nanoTime();
+					remainingTime = remainingTime/1000000;
+					if (remainingTime < 0){
+						remainingTime = 0;
+					}
+					Thread.sleep((long) remainingTime);
+					nextDrawTime += drawInterval;
+			}catch (InterruptedException e){
+				throw new RuntimeException(e);
+
+			}
+
+		}
+	}
+
+	public  void update(){
+		if (keyH.upPressed){
+			playerY -= playerSpeed;
+		}
+		else if (keyH.downPressed){
+			playerY += playerSpeed;
+		}
+
+		if (keyH.leftPressed){
+			playerX -= playerSpeed;
+		}
+		else if (keyH.rightPressed){
+			playerX += playerSpeed;
+		}
 
 	}
+	// PAINT WORLD
+	public  void paintComponent (Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setColor(Color.green);
+		g2.fillRect(playerX,playerY, tileSize, tileSize);
+		g2.dispose();
+
+	}
+
+
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -17,6 +17,9 @@ public class Main {
 		windows.setLocationRelativeTo(null);
 		windows.setVisible(true);
 
+
+		gamePanel.startGameThread();
+
 	}
 
 }
